280ae544b2ce47775d7a007a2b0f7d80f7daedb8
simon-gd <57882634+simon-gd@users.noreply.github.com>
2021-01-27 08:28:26 -0500

    a bit of cleanup

--- packages/server/modules/auth/strategies/azure.js
+++ packages/server/modules/auth/strategies/azure.js
@@ -2,48 +2,55 @@
 'use strict'
 
 const passport = require('passport')
-//const OIDCStrategy = require('passport-azure-ad').OIDCStrategy
-const AzureAdOAuth2Strategy  = require('passport-azure-ad-oauth2')
-const URL = require( 'url' ).URL
-const appRoot = require( 'app-root-path' )
-const { findOrCreateUser } = require( `${appRoot}/modules/core/services/users` )
+const AzureAdOAuth2Strategy = require('passport-azure-ad-oauth2')
+const URL = require('url').URL
+const appRoot = require('app-root-path')
+const { findOrCreateUser } = require(`${appRoot}/modules/core/services/users`)
 const jwt = require('jsonwebtoken');
 
 
-module.exports = ( app, session, sessionStorage, finalizeAuth ) => {
-    const strategy = {
-      id: 'azuread-openidconnect',
-      name: 'Azure Active Directory',
-      icon: 'mdi-microsoft-azure',
-      color: 'blue darken-3',
-      url: '/auth/azure',
-      callbackUrl: ( new URL( '/auth/azure/callback', process.env.CANONICAL_URL ) ).toString( )
-    }
+module.exports = (app, session, sessionStorage, finalizeAuth) => {
+  const strategy = {
+    id: 'azuread-openidconnect',
+    name: 'Microsoft Work/School Account',
+    icon: 'mdi-microsoft',
+    color: 'blue darken-3',
+    url: '/auth/azure',
+    callbackUrl: (new URL('/auth/azure/callback', process.env.CANONICAL_URL)).toString()
+  }
 
-   passport.use(new AzureAdOAuth2Strategy({
-      clientID: process.env.AZUREAD_CLIENT_ID,
-      clientSecret: process.env.AZUREAD_CLIENT_SECRET,
-      callbackURL: strategy.callbackUrl,
-      passReqToCallback: true,
-      useCommonEndpoint: true,
-    },
+  passport.use(new AzureAdOAuth2Strategy({
+    clientID: process.env.AZUREAD_CLIENT_ID,
+    clientSecret: process.env.AZUREAD_CLIENT_SECRET,
+    callbackURL: strategy.callbackUrl,
+    passReqToCallback: true,
+    useCommonEndpoint: true,
+  },
     async function (req, accessToken, refresh_token, params, profile, done) {
-      var profile = jwt.decode(params.id_token);
-      let email = profile.upn;
-      let name = profile.name;
-      let user = { email, name }
+      let tokenData = jwt.decode(params.id_token);
       
-      if ( req.session.suuid ) {
+      // UPN: UserPrincipalName Not a durable identifier for the user and should not be used to 
+      // uniquely identity user information (for example, as a database key). Instead, it's 
+      // better to use the user object ID (oid) as a database key. However in this case, we 
+      // need an email, and email might be empty
+      // Email: This value is included by default if the user is a guest in the tenant. 
+      // For managed users (the users inside the tenant), it must be requested through this 
+      // optional claim or, on v2.0 only, with the OpenID scope.
+      let email = tokenData.email || tokenData.upn;
+      let name = tokenData.name;
+      let user = { email, name }
+
+      if (req.session.suuid) {
         user.suuid = req.session.suuid
       }
-      let myUser = await findOrCreateUser( { user: user, rawProfile: profile } )
-      return done( null, myUser )
-      
-    }));
-    
-    app.get( strategy.url, session, sessionStorage, passport.authenticate( 'azure_ad_oauth2', { failureRedirect: '/auth/error' }  ) )
-    app.get( '/auth/azure/callback', session, passport.authenticate( 'azure_ad_oauth2', { failureRedirect: '/auth/error' } ), finalizeAuth );
-      
+      let myUser = await findOrCreateUser({ user: user, rawProfile: profile })
+      return done(null, myUser)
 
-    return strategy
-  }
+    }));
+
+  app.get(strategy.url, session, sessionStorage, passport.authenticate('azure_ad_oauth2', { failureRedirect: '/auth/error' }))
+  app.get('/auth/azure/callback', session, passport.authenticate('azure_ad_oauth2', { failureRedirect: '/auth/error' }), finalizeAuth);
+
+
+  return strategy
+}
